1514a09ceefc9a2feb9be225085af4f4bb887557
maneko00 <82707685+maneko00@users.noreply.github.com>
2021-11-22 07:45:01 +0900

    Add files via upload

---
+++ Views/HistoryView.cs
@@ -0,0 +1,16 @@
+﻿using DDD.Infrastructure.Fake;
+using DDD.WinForm.ViewModels;
+using System.Windows.Forms;
+
+namespace DDD.WinForm.Views
+{
+    public partial class HistoryView : Form
+    {
+        private ProductNumberHistoryViewModel view_model_
+            = new ProductNumberHistoryViewModel(new ProductNumberHistoryFake());
+        public HistoryView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Views/ProductNumberHistoryView.cs
@@ -0,0 +1,25 @@
+﻿using DDD.WinForm.ViewModels;
+using System.Windows.Forms;
+
+namespace DDD.WinForm.Views
+{
+    public partial class ProductNumberHistoryView : Form
+    {
+        private ProductNumberHistoryViewModel view_model_
+            = new ProductNumberHistoryViewModel();
+
+        public ProductNumberHistoryView()
+        {
+            InitializeComponent();
+
+            //テータバインディングは今回しなくていい
+            //this.BaseProductNumberTextBox.DadaBingings.Add(
+            //    "TextBox", view_model_, nameof(view_model_.BaseProductNumberText));
+        }
+
+        private void SaveButton_Click(object sender, System.EventArgs e)
+        {
+            view_model_.Save();
+        }
+    }
+}
