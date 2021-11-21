ffda5aea102f66d4ff9350d4f2dee58434be0332
maneko00 <82707685+maneko00@users.noreply.github.com>
2021-11-21 23:36:36 +0900

    Add files via upload

---
+++ Csv/ProductNumberCsv.cs
@@ -0,0 +1,21 @@
+﻿using DDD.Domain.Entities;
+using DDD.Domain.Repositories;
+
+namespace DDD.Infrastructure.Csv
+{
+    public class ProductNumberCsv : IProductNumberRipository
+    {
+        //public ProductNumberEntity GetProductNumber(string s, string sub)
+        //{
+            
+        //    return new ProductNumberEntity(
+        //                    s,
+        //                    sub);
+        //}
+
+        public void Save(ProductNumberEntity product_number)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
