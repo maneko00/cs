47fecfcb58ff2584b75cfa7e33ae6be6b8248297
maneko00 <82707685+maneko00@users.noreply.github.com>
2021-11-22 07:42:27 +0900

    Add files via upload

---
+++ DDD.Infrastructure/Fake/ProductNumberHistoryFake.cs
@@ -0,0 +1,18 @@
+﻿using DDD.Domain.Entities;
+using DDD.Domain.Repositories;
+
+namespace DDD.Infrastructure.Fake
+{
+    public class ProductNumberHistoryFake : IProductNumberRipository
+    {
+        public ProductNumberEntity GetHistory()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Save(ProductNumberEntity product_number)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
