0ab4337f33bb4ace9757b2db4ae7c101423e4b08
maneko00 <82707685+maneko00@users.noreply.github.com>
2021-11-21 23:38:01 +0900

    Add files via upload

---
+++ ViewModels/ProductNumberHistoryViewModel.cs
@@ -0,0 +1,38 @@
+﻿using DDD.Domain.Entities;
+using DDD.Domain.Helpers;
+using DDD.Domain.Repositories;
+using System;
+
+namespace DDD.WinForm.ViewModels
+{
+    public class ProductNumberHistoryViewModel : ViewModelBase
+    {
+        private IProductNumberRipository product_number_;
+
+        public ProductNumberHistoryViewModel(
+            IProductNumberRipository product_number
+            )
+        {
+            product_number_ = product_number;
+
+            //GetProductNumber = GetProductNumber();
+            BaseProductNumberText = string.Empty;
+            SubProductNumberText = string.Empty;
+        }
+
+        public string BaseProductNumberText { get; set; }
+        public string SubProductNumberText { get; set; }
+
+        public void Save()
+        {
+            Guard.IsStringEmpty(BaseProductNumberText, "入力してください");
+            Guard.IsStringEmpty(SubProductNumberText, "入力してください");
+
+            var entity = new ProductNumberEntity(
+                BaseProductNumberText,
+                SubProductNumberText);
+
+            product_number_.Save(entity);
+        }
+    }
+}
