ff06c4fb7adc65c3ef045207e2e8757e99efb5a1
maneko00 <82707685+maneko00@users.noreply.github.com>
2021-11-21 22:53:02 +0900

    Add files via upload

---
+++ Helpers/Guard.cs
@@ -0,0 +1,15 @@
+﻿using DDD.Domain.Exceptions;
+
+namespace DDD.Domain.Helpers
+{
+    public static class Guard
+    {
+        public static void IsStringEmpty(string o, string message)
+        {
+            if(o == "")
+            {
+                throw new InputException(message);
+            }
+        }
+    }
+}
---
+++ ViewModelTests/ProductNumberHistoryViewModelTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using DDD.Domain.Entities;
+using DDD.Domain.Exceptions;
+using DDD.Domain.Repositories;
+using DDD.WinForm.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace DDDTest.Tests
+{
+    [TestClass]
+    public class ProductNumberHistoryViewModelTest
+    {
+        [TestMethod]
+        public void 登録シナリオ()
+        {
+            var product_number_mock = new Mock<IProductNumberRipository>();
+
+            var viewModel = new ProductNumberHistoryViewModel();
+            viewModel.BaseProductNumberText.Is("");
+            viewModel.SubProductNumberText.Is("");
+
+            var ex = AssertEx.Throws<InputException>(() => viewModel.Save());
+            ex.Message.Is("入力してください");
+
+            viewModel.BaseProductNumberText = "000-0001";
+            viewModel.SubProductNumberText = "000-0002";
+
+            product_number_mock.Setup(x => x.Save(It.IsAny<ProductNumberEntity>())).
+                Callback<ProductNumberEntity>(saveValue =>
+                {
+                    saveValue.BaseProductNumber.Is("000-0001");
+                    saveValue.SubProductNumber.Is("000-0002");
+                });
+            viewModel.Save();
+
+
+
+            //viewModel.Search();
+            //viewModel.SearchProductNumberText.Is("010-1000");
+
+
+        }
+    }
+}
