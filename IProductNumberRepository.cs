4e0441137efb73ec56e5952d5d85f97490299917
maneko00 <82707685+maneko00@users.noreply.github.com>
2021-11-22 07:41:26 +0900

    Add files via upload

---
+++ IProductNumberRepository.cs
@@ -0,0 +1,10 @@
+﻿using DDD.Domain.Entities;
+
+namespace DDD.Domain.Repositories
+{
+    public interface IProductNumberRipository
+    {
+        ProductNumberEntity GetHistory();
+        void Save(ProductNumberEntity product_number);
+    }
+}
