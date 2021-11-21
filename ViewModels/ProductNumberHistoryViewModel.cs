a0bfdd03bc5188e5fba2e22b8ca63b2aa17909f7
maneko00 <82707685+maneko00@users.noreply.github.com>
2021-11-22 07:44:18 +0900

    Add files via upload

--- ViewModels/ProductNumberHistoryViewModel.cs
+++ ViewModels/ProductNumberHistoryViewModel.cs
@@ -1,13 +1,24 @@
 ﻿using DDD.Domain.Entities;
 using DDD.Domain.Helpers;
 using DDD.Domain.Repositories;
+using DDD.Infrastructure.Csv;
 using System;
+using System.Text;
+using System.Windows.Forms;
 
 namespace DDD.WinForm.ViewModels
 {
     public class ProductNumberHistoryViewModel : ViewModelBase
     {
         private IProductNumberRipository product_number_;
+
+        // 引数なしできたらコンストラクタ（引数あり）にnew ProductNumberCsvを投げる
+        public ProductNumberHistoryViewModel()
+            :this(new ProductNumberCsv())
+        {
+            // コンストラクタが二つになる
+            // product_number_ = new ProductNumberCsv();
+        }
 
         public ProductNumberHistoryViewModel(
             IProductNumberRipository product_number
@@ -27,6 +38,35 @@ namespace DDD.WinForm.ViewModels
         {
             Guard.IsStringEmpty(BaseProductNumberText, "入力してください");
             Guard.IsStringEmpty(SubProductNumberText, "入力してください");
+
+            // 確認画面
+            DialogResult dialogResult = MessageBox.Show(
+                  "保存しますか？",
+                  "確認",
+                  MessageBoxButtons.OKCancel,
+                  MessageBoxIcon.Question);
+
+            if (dialogResult == DialogResult.OK)
+            {
+                using (var sw = new
+                    System.IO.StreamWriter(
+                    @"",
+                    true,
+                    Encoding.GetEncoding("shift_jis")))
+                {
+                    sw.Write(BaseProductNumberText);
+                    sw.Write(",");
+                    sw.Write(SubProductNumberText);
+                    sw.WriteLine("");
+                }
+
+                //StatusLabelText = "保存しました";
+            }
+            else
+            {
+                //StatusLabelText = "キャンセルしました";
+            }
+
 
             var entity = new ProductNumberEntity(
                 BaseProductNumberText,
