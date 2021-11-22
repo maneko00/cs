579ec8fdf2fa2e3518367ab326259f9ade8fc086
maneko00 <82707685+maneko00@users.noreply.github.com>
2021-11-23 06:43:02 +0900

    Add files via upload
    データバインド

---
+++ _WeatherLatestView.cs
@@ -0,0 +1,58 @@
+﻿using DDD.Domain.Entities;
+using DDD.WinForm.ViewModels;
+using DDD.WinForm.Views;
+using System;
+using System.Windows.Forms;
+
+namespace DDD.WinForm
+{
+    public partial class WeatherLatestView : Form
+    {
+        private WeatherLatestViewModel _viewModel 
+            = new WeatherLatestViewModel();
+        public WeatherLatestView()
+        {
+            InitializeComponent();
+
+
+            //ここからデータバインディング
+            this.AreasComboBox.DataBindings.Add(
+                "Text", _viewModel, "AreaIdTextBox");
+            // this.AreasComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+            // this.AreasComboBox.DataBindings.Add(
+            //     "SelectedValue", _viewModel, ""(_viewModel.SelectedAreaId));
+            // this.AreasComboBox.DataBindings.Add(
+            //     "DataSource", _viewModel, nameof(_viewModel.Areas));
+            // this.AreasComboBox.ValueMember = nameof(AreaEntity.AreaId);
+            // this.AreasComboBox.DisplayMember = nameof(AreaEntity.AreaName);
+
+            // this.DataDateLabel.DataBindings.Add(
+            //    "Text", _viewModel, nameof(_viewModel.DataDateText));
+            // this.ConditionLabel.DataBindings.Add(
+            //    "Text", _viewModel, nameof(_viewModel.ConditionText));
+            // this.TemperatureLabel.DataBindings.Add(
+            //    "Text", _viewModel, nameof(_viewModel.TemperatureText));
+        }
+
+        private void LatestButton_Click(object sender, EventArgs e)
+        {
+            _viewModel.Search();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var f = new WeatherListView())
+            {
+                f.ShowDialog();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (var f = new WeatherSaveView())
+            {
+                f.ShowDialog();
+            }
+        }
+    }
+}
